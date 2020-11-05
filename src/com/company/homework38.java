acc67ea66193d8edc610cc26c0a10c00e0f3cfb8
Gevorg <hovhannisyangevorg744@gmail.com>
2020-11-05 10:21:55 +0300

    hello

---
+++ src/com/company/homework38.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class homework38 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+        int number4 = scanner.nextInt();
+        int number5 = scanner.nextInt();
+        int number6 = scanner.nextInt();
+        int number7 = scanner.nextInt();
+        int number8 = scanner.nextInt();
+        int number9 = scanner.nextInt();
+        int number10 = scanner.nextInt();
+        int sum = number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10;
+        System.out.println(sum);
+    }
+}
